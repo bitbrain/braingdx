19118231bb7c1e2c4b552f8628d653ab50622e79
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2015-07-20 19:20:19 +0200

    Introduced BrainGdx game

---
+++ core/src/de/bitbrain/braingdx/BrainGdxGame.java
@@ -0,0 +1,16 @@
+package de.bitbrain.braingdx;
+
+import com.badlogic.gdx.Game;
+
+/**
+ * Base implementation for BrainGDX games
+ *
+ * @author Miguel Gonzalez <miguel-gonzalez@gmx.de>
+ */
+public class BrainGdxGame extends Game {
+
+    @Override
+    public void create() {
+
+    }
+}
