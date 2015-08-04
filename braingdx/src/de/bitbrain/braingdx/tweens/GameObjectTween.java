0ee1fb5d7848fff04614ea880d32ce6d905eb67f
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2015-08-04 22:03:48 +0200

    Implemented color management for game objects

--- braingdx/src/de/bitbrain/braingdx/tweens/GameObjectTween.java
+++ braingdx/src/de/bitbrain/braingdx/tweens/GameObjectTween.java
@@ -30,14 +30,17 @@ import de.bitbrain.braingdx.GameObject;
  */
 public class GameObjectTween implements TweenAccessor<GameObject> {
 
-    public static final int SCALE_X = 1;
-    public static final int SCALE_Y = 2;
-    public static final int POS_X = 3;
-    public static final int POS_Y = 4;
-    public static final int WIDTH = 5;
-    public static final int HEIGHT = 6;
-    public static final int ALPHA = 7;
-    public static final int SCALE = 8;
+    public static final int SCALE_X = 100;
+    public static final int SCALE_Y = 102;
+    public static final int POS_X = 103;
+    public static final int POS_Y = 104;
+    public static final int SCALE = 105;
+    public static final int ALPHA = ColorTween.A;
+    public static final int R = ColorTween.R;
+    public static final int G = ColorTween.G;
+    public static final int B = ColorTween.B;
+
+    private ColorTween colorTween = new ColorTween();
 
     @Override
     public int getValues(GameObject target, int tweenType, float[] returnValues) {
@@ -57,6 +60,9 @@ public class GameObjectTween implements TweenAccessor<GameObject> {
             case POS_Y:
                 returnValues[0] = target.getTop();
                 return 1;
+            case ALPHA: case R:
+            case G: case B:
+                return colorTween.getValues(target.getColor(), tweenType, returnValues);
         }
         return 0;
     }
@@ -79,6 +85,10 @@ public class GameObjectTween implements TweenAccessor<GameObject> {
                 break;
             case POS_Y:
                 target.setPosition(target.getLeft(), newValues[0]);
+                break;
+            case ALPHA: case R:
+            case G: case B:
+                colorTween.setValues(target.getColor(), tweenType, newValues);
                 break;
         }
     }
