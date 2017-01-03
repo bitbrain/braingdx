1a5394f34f7c8c9593ea1772c73b893bf8fb513c
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2017-01-03 15:42:37 +0000

    Change License from 2016 -> 2017

--- core/src/main/java/de/bitbrain/braingdx/util/Resizeable.java
+++ core/src/main/java/de/bitbrain/braingdx/util/Resizeable.java
@@ -1,4 +1,4 @@
-/* Copyright 2016 Miguel Gonzalez Sanchez
+/* Copyright 2017 Miguel Gonzalez Sanchez
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
