2bb876d5dfef0cdf6f56ca0b4fad8dada1fdeac4
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2017-05-19 11:48:29 +0100

    Revert "Try to dodge SSL"
    Do not dodge, letsencrypt is just down.
    
    This reverts commit 10863b049eb055ec0b1694f941a62101e9bcaf10.

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -3,6 +3,5 @@
 if [ "$TRAVIS_BRANCH" = 'deploy' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
 mvn deploy \
     -DskipTests \
-    --settings deployment/settings.xml \
-    -Dmaven.wagon.http.ssl.insecure=true
+    --settings deployment/settings.xml
 fi
