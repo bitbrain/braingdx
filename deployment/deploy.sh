2c844eb971901c382a0e6f66d6ea0c67bbcde8af
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2017-05-19 14:38:41 +0100

    Ignore SSL validity dates

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -3,5 +3,8 @@
 if [ "$TRAVIS_BRANCH" = 'deploy' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
 mvn deploy \
     -DskipTests \
+    -Dmaven.wagon.http.ssl.ignore.validity.dates=true \
+    -Dmaven.wagon.http.ssl.insecure=true \
+    -Dmaven.wagon.http.ssl.allowall=true \
     --settings deployment/settings.xml
 fi
