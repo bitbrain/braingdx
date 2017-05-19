10863b049eb055ec0b1694f941a62101e9bcaf10
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2017-05-19 11:42:15 +0100

    Try to dodge SSL

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -3,5 +3,6 @@
 if [ "$TRAVIS_BRANCH" = 'deploy' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
 mvn deploy \
     -DskipTests \
-    --settings deployment/settings.xml
+    --settings deployment/settings.xml \
+    -Dmaven.wagon.http.ssl.insecure=true
 fi
