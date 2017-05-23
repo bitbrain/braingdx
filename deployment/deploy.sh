3b3456d8f094a7ae22d60de2f359150de10cd2f2
Miguel ☕️ <miguel-gonzalez@gmx.de>
2017-05-23 20:28:28 +0100

    Update deploy.sh

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -3,8 +3,5 @@
 if [ "$TRAVIS_BRANCH" = 'deploy' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
 mvn deploy \
     -DskipTests \
-    -Dmaven.wagon.http.ssl.ignore.validity.dates=true \
-    -Dmaven.wagon.http.ssl.insecure=true \
-    -Dmaven.wagon.http.ssl.allowall=true \
     --settings deployment/settings.xml
 fi
