65af96bc73f9d893ba1b8f631a80e195a6c5367f
Miguel Gonzalez Sanchez <miguel-gonzalez@gmx.de>
2018-07-10 22:38:12 +0100

    Fixing script

--- deployment/after-deploy.sh
+++ deployment/after-deploy.sh
@@ -5,16 +5,42 @@ if [ "$LATEST_TAG" = "$CH_VERSION" ]; then
    exit 0
 fi
 
-echo "Deploying Javadoc to Github pages..."
-
 # 1. Download freshly from Github
 cd $HOME
-git config --global user.email "travis@travis-ci.org"
-git config --global user.name "travis-ci"
-git clone --quiet --branch=master https://$GITHUB_TOKEN@github.com/bitbrain/braingdx
+git config --global user.email "jarvisdeploybot@gmail.com"
+git config --global user.name "Jarvis"
+git clone --quiet --branch=master https://${GITHUB_TOKEN}@github.com/bitbrain/braingdx
 
-# 2. Publish new release to Github
-source "github-release.sh"
+curl -X POST \
+-u bitbrain:$GITHUB_TOKEN \
+-d "{\
+  \"tag_name\": \"$CH_VERSION\",\
+  \"target_commitish\": \"$TRAVIS_BRANCH\",\
+  \"name\": \"Version $CH_VERSION\",\
+  \"body\": \"$CH_TEXT\",\
+  \"draft\": false,\
+  \"prerelease\": false\
+}" https://api.github.com/repos/bitbrain/braingdx/releases
 
-# 3. Push Javadoc to Github
-source "github-javadoc.sh"
+mkdir cd $HOME/docs
+cd $HOME/braingdx
+mvn versions:set -DnewVersion=$CH_VERSION && mvn javadoc:javadoc
+cd $HOME/docs
+cp -r $HOME/braingdx/core/target/site/apidocs/* $HOME/docs
+rm -rf $HOME/braingdx/*
+cd $HOME/braingdx
+
+# 4. Checkout Jekyll branch and create new folder with new version
+git checkout gh-pages
+mkdir -p $HOME/braingdx/docs/$CH_VERSION
+cp -r $HOME/docs/* $HOME/braingdx/docs/$CH_VERSION
+
+# 5. Copy also into "latest" docs
+rm -rf $HOME/braingdx/docs/latest
+mkdir -p $HOME/braingdx/docs/latest
+cp -r $HOME/docs/* $HOME/braingdx/docs/latest
+
+# 6. Add everything and push!
+git add -f *
+git commit -m "Travis build $TRAVIS_BUILD_NUMBER - update Javadoc"
+git push -fq origin gh-pages && echo "Successfully deployed Javadoc to /docs"
