9920be0ed2b6876508f050d5a7aafa8d1e9d3e1b
Miguel Gonzalez Sanchez <miguel-gonzalez@gmx.de>
2019-03-18 19:25:53 +0000

    Remove demo project

--- demo/assets/rpg/test.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="tilesheet_complete" tilewidth="111" tileheight="128" tilecount="168" columns="21">
- <image source="tilesheet_complete.png" width="2331" height="1025"/>
-</tileset>
--- demo/desktop/src/main/java/de/bitbrain/braingdx/demo/DemoConfig.java
+++
@@ -1,8 +0,0 @@
-package de.bitbrain.braingdx.demo;
-
-public interface DemoConfig {
-
-   String APP_NAME = "BrainGDX Testbench";
-   String APP_VERSION = "1.0.0";
-
-}
